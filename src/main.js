757fcb8ed130389ee877be25e41f350a33eb2335
mcnadams <mcneilbo@oregonstate.edu>
2019-02-01 14:54:56 -0800

    limits number of wrong guesses before lose message is dynamically displayed and guess form is hidden

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ const guessForm = document.getElementById('guess-form');
 const rightGuessed = document.getElementById('right');
 const wrongGuessed = document.getElementById('wrong');
 const secretWordNode = document.getElementById('secret-word');
+const winMessage = document.getElementById('win');
+const loseMessage = document.getElementById('lose');
+const announceResult = document.getElementById('announce-result');
+
+let wrongGuessCount = 0;
+const maxWrongGuesses = 3;
 
 const words = [
     'gallows',
@@ -35,8 +41,18 @@ guessForm.addEventListener('submit', function(event) {
     }
     else {
         wrongGuessed.textContent += guess;
+        wrongGuessCount++;
+        if(wrongGuessCount > maxWrongGuesses) {
+            guessForm.classList.add('hidden');
+            const newMessage = document.createElement('h2');
+            newMessage.classList.add('lose');
+            newMessage.textContent = 'Sorry, you\'re out of chances. See you in hell.';
+            announceResult.appendChild(newMessage);
+        }
     }
 });
+
+
  
 
 
@@ -47,14 +63,10 @@ guessForm.addEventListener('submit', function(event) {
 //add a part to the gallows if a wrong letter is guessed
 
 
-//track number of wrong guesses and lose if max is reached
-
-
-//win/lose message at the top
+//win message at the top
 
 
 //choose a random word from words array
 
 
 //if the same letter is guessed twice, don't add it to the letters guessed again
-
